609b0c0969d37109c24292b2c545c64b2db6a000
omergershonwork-commits <omergershonwork@gmail.com>
2026-09-09 18:19:47 +0300

    Simplify country properties with CountryInfo

--- src/main/java/com/ab/orders/config/CountryProperties.java
+++ src/main/java/com/ab/orders/config/CountryProperties.java
@@ -1,44 +1,20 @@
 package com.ab.orders.config;
 
+import com.ab.orders.domain.CountryInfo;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 
-/** Binds the country reference-data section from application.yml. */
+/** Binds the country reference-data section from the dedicated country config file. */
+@Getter
+@Setter
 @Component
 @ConfigurationProperties(prefix = "reference-data")
 public class CountryProperties {
 
-    private Map<String, Country> countries = new HashMap<>();
-
-    public Map<String, Country> getCountries() {
-        return countries;
-    }
-
-    public void setCountries(Map<String, Country> countries) {
-        this.countries = countries;
-    }
-
-    public static class Country {
-        private String name;
-        private String currency;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getCurrency() {
-            return currency;
-        }
-
-        public void setCurrency(String currency) {
-            this.currency = currency;
-        }
-    }
+    private Map<String, CountryInfo> countries = new HashMap<>();
 }
